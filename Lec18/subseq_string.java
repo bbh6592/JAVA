e148ca51b9606a5943d015ed402036ead27dd836
Vinaya Redekar <91121141+bbh6592@users.noreply.github.com>
2024-04-12 16:26:37 +0530

    Add files via upload

---
+++ Lec18/subseq_string.java
@@ -0,0 +1,19 @@
+package Lec18;
+
+public class subseq_string {
+    public static void printSubseq(String str, int idx, String res) {
+        if(idx == str.length()) {
+            System.out.println(res);
+            return;
+        }
+        //choose
+    printSubseq(str, idx+1, res+str.charAt(idx));
+        //don't choose
+    printSubseq(str, idx+1, res);
+    }
+    public static void main(String args[]) {
+        String str1 = "abc";
+        String str2 = "aaa";
+        printSubseq(str1, 0, "");
+    }
+}
