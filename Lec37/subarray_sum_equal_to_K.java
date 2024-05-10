b65b7bc36d3a70ef01beeae8e8b08615ce41c7ed
Vinaya Redekar <91121141+bbh6592@users.noreply.github.com>
2024-05-10 22:21:56 +0530

    Add files via upload

---
+++ Lec37/subarray_sum_equal_to_K.java
@@ -0,0 +1,28 @@
+package Lec37;
+import java.util.*;
+public class subarray_sum_equal_to_K {
+    public static void main(String[] args) {
+        int arr[] = {10,2,-2,-20,10}; //ans=3
+        int k = -10;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        map.put(0,1); //empty subarray
+        int ans = 0;
+        int sum = 0;
+        for(int j=0; j<arr.length; j++) {
+            sum += arr[j];
+
+            if(map.containsKey(sum-k)) {
+                ans += map.get(sum-k);
+            }
+
+            if(map.containsKey(sum)) {
+                map.put(sum, map.get(sum) + 1);
+            } else {
+                map.put(sum, 1);
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
